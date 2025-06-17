d0ffc3328b56f49bed37968c5eb2294d54779c7f
eliasylonen <elias.ylonen@gmail.com>
2025-06-17 04:33:56 +0200

    Add image

--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,48 @@
 import { PropsWithChildren } from "react";
 import { Footer } from "./Footer";
 import { WhatsAppButton } from "./WhatsAppButton";
+import Image from "next/image";
+import Link from "next/link";
 
-const P = ({ children }: PropsWithChildren) => (
-  <p className="mb-5">{children}</p>
+const P = ({
+  children,
+  className,
+}: PropsWithChildren & { className?: string }) => (
+  <p className={className ?? "mb-5"}>{children}</p>
 );
 
 export default function Home() {
   return (
-    <div className="flex flex-col font-[family-name:var(--font-ibm-plex-sans)] min-h-screen">
+    <div className="flex flex-col font-[family-name:var(--font-ibm-plex-sans)] min-h-screen text-zinc-800">
       <header className="flex-1 flex justify-center">
         <div className="flex-1 flex items-center max-w-2xl px-5 h-32">
-          nomadia.club
+          <Link href={"/"} className="active:scale-95">
+            nomadia.club
+          </Link>
         </div>
       </header>
       <main className="flex-1 flex flex-col items-center">
         <section>
-          <div className="max-w-2xl px-5">
+          <div className="max-w-2xl px-5 pb-16">
             <P>Hi, I'm Elias!</P>
-            <P>I host workation trips for the most kind and driven people.</P>
+            <Image
+              className="mb-5 object-cover overflow-hidden w-64 h-64"
+              src="/images/elias.jpg"
+              width={256}
+              height={256}
+              alt="Elias Ylönen in Chicago"
+            />
+            <P>
+              I host workation <b>trips for</b> the most{" "}
+              <b>kind and driven people</b>.
+            </P>
             <P>
               The next trip will be an extended weekend in Croatia in the end of
               August. I'll lock in the dates asap and welcome the first people
               to join!
             </P>
-            <P>There is</P>
-            <ul className="pl-1">
+            <P className="mb-0">There is</P>
+            <ul className="pl-1 pb-5">
               <li>
                 - A super beautiful location e.g. a seaside villa for everyone
               </li>
@@ -40,7 +57,7 @@ export default function Home() {
               <li>- No schedule and full freedom to relax and work</li>
             </ul>
             <P>
-              If you might be interested, text me! Good ideas and questions also
+              If you'd like to join, text me! Good ideas and questions also
               always welcome.
             </P>
             <WhatsAppButton />
